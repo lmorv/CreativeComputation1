f429b20a12ec1ba306694bbe4df30f6f0f1ebd9a
lmorv <84216711+lmorv@users.noreply.github.com>
2021-09-09 20:58:13 -0400

    Refining shape colors

--- experiments/drawing experiments/js/script.js
+++ experiments/drawing experiments/js/script.js
@@ -29,13 +29,25 @@ function setup(){
   point(250, 250);
 
   rectMode(CENTER);
+  fill(220, 0, 200, 100)  //set the color of the rectangle
+  noStroke()
   rect(250, 250, 250, 250);
+  rect(250, 250, 220, 220);
+  rect(250, 250, 100, 100);
 
   line(0, 0, 500, 500);
   line(500, 0, 0, 500);
 
+  fill(255, 127, 80)  //set the color of the elipses
+  stroke(255, 255, 0) // fill & stroke instructions override the previous fill & stroke instructions
+
   ellipseMode(CORNER);
   ellipse(250, 250, 100, 100);
+  ellipse(250, 250, 80, 80);
+  ellipse(250, 250, 60, 60);
+  ellipse(250, 250, 60, 60);
+  ellipse(250, 250, 40, 40);
+  ellipse(250, 250, 40, 40);
 }
 
 
