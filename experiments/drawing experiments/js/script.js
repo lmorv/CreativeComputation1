e278461d29f7ca1d0522c3df41a1bddbc98c2b6f
lmorv <84216711+lmorv@users.noreply.github.com>
2021-09-10 00:31:30 -0400

    adjusting alpha

--- experiments/drawing experiments/js/script.js
+++ experiments/drawing experiments/js/script.js
@@ -29,7 +29,7 @@ function setup(){
   point(250, 250);
 
   rectMode(CENTER);
-  fill(220, 0, 200, 100)  //set the color of the rectangle
+  fill(220, 0, 200, 100)  //set the color of the rectangles
   noStroke()
   rect(250, 250, 250, 250);
   rect(250, 250, 220, 220);
@@ -38,7 +38,7 @@ function setup(){
   line(0, 0, 500, 500);
   line(500, 0, 0, 500);
 
-  fill(255, 127, 80)  //set the color of the elipses
+  fill(255, 127, 80, 100)  //set the color of the elipses
   stroke(255, 255, 0) // fill & stroke instructions override the previous fill & stroke instructions
 
   ellipseMode(CORNER);
