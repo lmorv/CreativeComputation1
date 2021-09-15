22bd1f38e4b1cae9f7b00d59ad4c5121a1d25f48
lmorv <84216711+lmorv@users.noreply.github.com>
2021-09-15 02:21:28 -0400

    enclosed distinct circles, implemented map & constrain

--- experiments/variables explorartion/js/script.js
+++ experiments/variables explorartion/js/script.js
@@ -9,12 +9,8 @@ Experimenting with variables
 // Variable declarations
 let backgroundShade = 0;
 
-// let circleX = 0;
-// let circleY = 225;
-// let circleSize = 200;
-// let circleSpeed = 2;
-
-let circle = {
+// flickering circle
+let circle1 = {
   x: 250,
   y: 250,
   size: 150,
@@ -23,11 +19,19 @@ let circle = {
   count: 0
 };
 
+// moving circle
+let circle2 = {
+  x: 0,
+  y: 250,
+  size: 150,
+  speed: 0.5,
+  fill: 255
+};
+
 /********
 Description of preload
 *********/
 function preload() {
-
 }
 
 /************
@@ -41,20 +45,38 @@ function setup() {
 Description of draw()
 **********/
 function draw() {
-
   background(backgroundShade);
 
-  circle.count = circle.count + .0025;
-  circle.speed = random(-5, 5);
-  circle.x = random(125, height-125);
-  circle.y = random(125, height-125);
-  circle.size = sin(circle.count) * 160;
+// flickering circle
+  push();
+  circle1.count = circle1.count + .0025;
+  circle1.speed = random(-5, 5);
+  circle1.x = random(125, height-125);
+  circle1.y = random(125, height-125);
+  circle1.size = sin(circle1.count) * 160;
 
-  circle.fill = random(0, 255);
-  fill(circle.fill);
-  ellipse(circle.x, circle.y, circle.size);
+  circle1.fill = random(0, 255);
+  fill(circle1.fill);
+// Draw an ellipse using circle1
+  ellipse(circle1.x, circle1.y, circle1.size);
+  pop();
 
+// moving circle
+push();
+circle2.x = circle2.x + circle2.speed;
+circle2.x = constrain(circle2.x, 0, width) // prevent the circle from exiting the canvas
+
+circle2.size = map(mouseY, height, 0, 100, 500) // mapped circle size to mouse Y position
+
+circle2.fill = map(circle2.x, 125, width, 0, 255) // maped shade to circle's X possition
+
+noStroke();
+fill(circle2.fill);
+ellipse(circle2.x, circle2.y, circle2.size);
+pop();
+
+// local variable declaration, just for debuging example
   let randomNumber = random();
-
+// print the value of a variabe like so:
 console.log(`randomNumber: ${randomNumber}`);
 }
