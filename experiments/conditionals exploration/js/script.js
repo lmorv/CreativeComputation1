25b1ee35b7fb760f4eccccc700ebf09e60394e4c
lmorv <84216711+lmorv@users.noreply.github.com>
2021-09-16 22:21:42 -0400

    trying out some examples.

--- experiments/conditionals exploration/js/script.js
+++ experiments/conditionals exploration/js/script.js
@@ -7,7 +7,7 @@ author, and this description to match your project!
 */
 
 "use strict";
-
+let angle = 0;
 
 /**
 Description of preload
@@ -15,19 +15,29 @@ Description of preload
 function preload() {
 
 }
-
-
 /**
 Description of setup
 */
 function setup() {
-
+  createCanvas(400, 400);
 }
-
 
 /**
 Description of draw()
 */
 function draw() {
+  // Note we don't fill the background
+  // No stroke because it's pretty
+  noStroke();
+  // Calculate the current sine of our angle and map it to a
+  // shape between 100 and 255
+  let shade = map(sin(angle), -1, 1, 100, 255);
+  // Set the fill to the shade
+  fill(shade);
+  // Draw a circle at the mouse position
+  ellipse(mouseX, mouseY, 20);
+  // Increase the angle so the sine function returns a different
+  // value next time
+  angle += 0.2;
 
 }
