b4c4556ebff75feee9b1e7ea0243b0a5898cdb9e
lmorv <84216711+lmorv@users.noreply.github.com>
2021-09-17 23:32:45 -0400

    Added teeth to alien using looops

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -7,7 +7,11 @@ A very good picture of an alien
 
 "use strict";
 
-
+let teeth = {
+  x: 280,
+  y: 385,
+  toothSize: 11,
+}
 /**
 Description of preload
 */
@@ -20,34 +24,54 @@ function preload() {
 Draws the face of an Alien
 */
 function setup() {
-  createCanvas(640,480);
+  createCanvas(640, 480);
 
   background(255, 100, 100);
   noStroke();
 
   //Draw the body
+  push();
   fill(127);
-  ellipse(320,480,300,200);
+  ellipse(320, 480, 300, 200);
+  pop();
 
   //Draw the head
+  push();
   fill(100);
-  ellipse(320,240,250,400);
+  ellipse(320, 240, 250, 400);
+  pop();
 
   //Draw the eyes
+  push();
   fill(0);
-  ellipse(250, 240, 80, 250);
-  ellipse(390, 240, 80, 250);
+  ellipse(250, 240, 80, 240);
+  ellipse(390, 240, 80, 240);
+  pop();
 
   //Draw the nostrils
+  push();
   fill(0);
-  ellipse(300,350,10,10);
-  ellipse(340,350,10,10);
+  ellipse(300, 350, 10, 10);
+  ellipse(340, 350, 10, 10);
+  pop();
 
   //Draw the mouth
-  stroke(50,50,200);
+  push();
+  stroke(50, 50, 200);
+  fill(0);
   strokeWeight(5);
   rectMode(CENTER);
-  rect(320,390,100,25);
+  rect(320, 390, 120, 30, 20);
+  pop();
+
+  // Draw the teeth
+  let x = teeth.x;
+  let numTeeth = 10
+
+  for (let i = 0; i < numTeeth; i++) {
+    ellipse(x, teeth.y, teeth.toothSize);
+    x = x + 9;
+  }
 }
 
 
