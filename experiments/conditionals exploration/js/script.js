f7c386d6c0b503b709be1a80625b8b31a7eee9cd
lmorv <84216711+lmorv@users.noreply.github.com>
2021-09-17 22:00:13 -0400

    Learning about loops!

--- experiments/conditionals exploration/js/script.js
+++ experiments/conditionals exploration/js/script.js
@@ -7,7 +7,11 @@ Experimenting with conditionals, yaaaaaay!
 
 "use strict";
 
-let displayCircle = false;
+let caterpillar = {
+  x: 5,
+  y: 250,
+  segmentSize: 20,
+}
 /**
  preload
 */
@@ -26,12 +30,25 @@ Description of draw()
 */
 function draw() {
   background(0);
+  noStroke();
+  fill(100, 200, 100);
+  // while loop:
+  // let x = caterpillar.x;
+  // let numSegments = 20;
+  // let segmentsDrawn = 0;
+  //
+  // while (segmentsDrawn < numSegments) {
+  //   ellipse(x, caterpillar.y, caterpillar.segmentSize);
+  //   x = x + 18;
+  //   segmentsDrawn++;
 
-  if (mouseIsPressed) {
-    displayCircle = true;
+  // for loop
+  let x = caterpillar.x;
+  let numSegments = 10
+
+  for (let i = 0; i < numSegments; i++) {
+    ellipse(x, caterpillar.y, caterpillar.segmentSize);
+    x = x + 20;
   }
 
-  if (displayCircle) {
-    ellipse(250, 250, 100, 100);
-  }
 }
