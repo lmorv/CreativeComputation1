52a7e6b48f287a0e931fef48c7eb1959cd847e48
lmorv <84216711+lmorv@users.noreply.github.com>
2021-09-20 01:31:49 -0400

    starting dodging activity

--- activities/04-dodging-covid19/js/script.js
+++ activities/04-dodging-covid19/js/script.js
@@ -8,6 +8,19 @@ author, and this description to match your project!
 
 "use strict";
 
+let rect1 = {
+  x: 0,
+  y: 0,
+  angle: 0,
+  speed: 0.01,
+}
+
+let rect2 = {
+  x: 0,
+  y: 0,
+  angle: 0,
+  speed: 0.01,
+}
 
 /**
 Description of preload
@@ -15,19 +28,43 @@ Description of preload
 function preload() {
 
 }
-
-
 /**
 Description of setup
 */
 function setup() {
-
+  createCanvas(500, 500);
 }
-
-
 /**
 Description of draw()
 */
 function draw() {
+  background(127);
+
+
+
+  // back ground FX
+  // draw rectangle 1
+  push();
+  noStroke();
+  fill(255, 110, 50);
+  rectMode(CENTER);
+  translate(width / 2, height / 2)
+  rotate(rect1.angle);
+  scale(4);
+  rect(rect1.x, rect1.y, 100, 100);
+  pop();
+  // draw rectangle 2
+  push();
+  noStroke();
+  fill(255, 110, 50);
+  rectMode(CENTER);
+  translate(width / 2, height / 2)
+  rotate(rect2.angle);
+  scale(4);
+  rect(rect2.x, rect2.y, 100, 100);
+  pop();
+  // move rects in oposite directions
+  rect1.angle += rect1.speed;
+  rect2.angle += -rect2.speed;
 
 }
