216016895211d73f6a82829b673138ddd86eee94
lmorv <84216711+lmorv@users.noreply.github.com>
2021-09-21 18:12:00 -0400

    more return values experiments

--- experiments/functions exploration/js/script.js
+++ experiments/functions exploration/js/script.js
@@ -7,7 +7,13 @@ These are explorations on the subect of function creation. I will not go into fu
 
 "use strict";
 
-
+let circle = {
+  x: 250,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0
+}
 
 /**
 Description of preload
@@ -29,6 +35,7 @@ function setup() {
   let coldCelsius = toCelsius(10);
   console.log(`10 dagees Farenheit is ${coldCelsius} degrees Celsius.`);
 
+  reset();
 }
 
 /**
@@ -37,15 +44,30 @@ Description of draw()
 function draw() {
   background(0);
 
-  let x = random(0, width);
-  let y = random(0, height);
+  move();
 
-  ellipse(x, y, 100);
+  if (circle.x < 0 || circle.x > width || circle.y < 0 || circle.y > height) {
+    reset();
+  }
 
-  parallels(100, 300, 6, 3, 70, 5);
-  parallels(70, 250, 20, 2, 400, 15);
-  parallels(200, 200, 70, 6, 50, 8);
-  parallels(350, 400, 8, 0.5, 200, 10);
+  ellipse(circle.x, circle.y, circle.size);
+
+  // parallels(100, 300, 6, 3, 70, 5);
+  parallels(70, 250, 27, 2, 400, 15);
+  // parallels(200, 200, 70, 6, 50, 8);
+  // parallels(350, 400, 8, 0.5, 200, 10);
+}
+
+function move() {
+  circle.x = circle.x + circle.vx;
+  circle.y = circle.y + circle.vy;
+}
+
+function reset() {
+  circle.x = 250;
+  circle.y = 250;
+  circle.vx = random(-10, 10);
+  circle.vy = random(-10, 10);
 }
 
 function parallels(x, y, numLines, lineWidth, lineHeight, lineSpacing) {
