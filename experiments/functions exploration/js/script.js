9f5016aedc08f931308313451bf8e11f11ec8ce4
lmorv <84216711+lmorv@users.noreply.github.com>
2021-09-21 03:29:39 -0400

    Started functions exploration

--- experiments/functions exploration/js/script.js
+++ experiments/functions exploration/js/script.js
@@ -1,13 +1,19 @@
 /**
-Title of Project
-Author Name
+Functions
+Leonardo Morales
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+These are explorations on the subect of function creation. I will not go into furthe detail at the moment.
 */
 
 "use strict";
 
+let circle = {
+  x: 0,
+  y: 0,
+  size: 100,
+  vx: 1,
+  vy: 0
+}
 
 /**
 Description of preload
@@ -16,18 +22,23 @@ function preload() {
 
 }
 
-
 /**
 Description of setup
 */
+
 function setup() {
-
+  createCanvas(500, 500);
 }
-
 
 /**
 Description of draw()
 */
 function draw() {
+  background(0);
+
+  circle.x += circle.vx;
+  circle.y += circle.vy;
+
+  ellipse(circle.x, circle.y, circle.size);
 
 }
