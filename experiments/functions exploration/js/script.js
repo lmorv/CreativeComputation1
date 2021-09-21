002cb62bc116119b828c19f3903cbf3e214f98d7
lmorv <84216711+lmorv@users.noreply.github.com>
2021-09-21 03:54:38 -0400

    modularity!

--- experiments/functions exploration/js/script.js
+++ experiments/functions exploration/js/script.js
@@ -9,7 +9,7 @@ These are explorations on the subect of function creation. I will not go into fu
 
 let circle = {
   x: 0,
-  y: 0,
+  y: 220,
   size: 100,
   vx: 1,
   vy: 0
@@ -35,10 +35,34 @@ Description of draw()
 */
 function draw() {
   background(0);
+  move();
+  wrap();
+  displayShape();
+}
 
+function mousePressed() {
+  reset();
+}
+
+function move() {
   circle.x += circle.vx;
   circle.y += circle.vy;
+}
 
+function wrap() {
+  if (circle.x > width) {
+    reset();
+  }
+}
+
+function displayShape() {
+  fill(255, 0, 0);
   ellipse(circle.x, circle.y, circle.size);
+}
 
+function reset() {
+  circle.x = 0;
+  circle.vx = circle.vx + 1;
+  circle.vy = circle.vy + 0.25;
+  circle.size = circle.size + 5;
 }
