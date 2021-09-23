3065519e0778bfa795b9c72acf735b55a857c748
lmorv <84216711+lmorv@users.noreply.github.com>
2021-09-22 23:35:40 -0400

    started looking for love activity.

--- activities/05-looking-for-love/js/script.js
+++ activities/05-looking-for-love/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+Looking for love
+Leonardo Morales
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -8,6 +8,25 @@ author, and this description to match your project!
 
 "use strict";
 
+let circle1 = {
+  x: 0,
+  y: 0,
+  vx: 0,
+  vy: 0,
+  size: 100,
+  speed: 5,
+}
+
+let circle2 = {
+  x: 0,
+  y: 0,
+  vx: 0,
+  vy: 0,
+  size: 100,
+  speed: 5,
+}
+
+let state = title; // states: title, simulation, ending
 
 /**
 Description of preload
