bc719a427aa16b1e63ad5d482117cc5101440b41
lmorv <84216711+lmorv@users.noreply.github.com>
2021-09-29 12:06:27 -0400

    move-it: cleanup part 2, commenting

--- exercises/01-move-it!/js/script.js
+++ exercises/01-move-it!/js/script.js
@@ -69,10 +69,10 @@ function draw() {
   let mouseY3D = mouseY - height / 2;
   let mouseX3D = mouseX - height / 2;
 
-  // global rotation on 0, 0, 0
+  // global rotation on non-translated origin  ('world' 0, 0, 0)
+  // turn this off for less confusing user interaction
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
-
 
   // CUBE 1:
   push()
@@ -93,11 +93,12 @@ function draw() {
   //USER CUBE display:
   push();
   fill(userCube.fill.r, userCube.fill.g, userCube.fill.b);
-  translate(userCube.tx, userCube.ty); // move this box to mouse possition
+  translate(userCube.tx, userCube.ty);
   box(userCube.dim);
   pop();
 
   //USER CUBE movemnet:
+  // move user cube to mouse possition
   userCube.tx = mouseX3D;
   userCube.ty = mouseY3D;
   // constrain user cube translate to canvas (fixed) dimentions
@@ -106,23 +107,23 @@ function draw() {
 
 
   // handle scalar tranformations:
-  cube1.dim = map(mouseY3D, 0, height / 2, 0, mouseY - height / 2);
-  cube2.dim = map(mouseX3D, 0, width / 2, 0, mouseX - width / 2);
+  cube1.dim = map(mouseY3D, 0, height / 2, 0, mouseY - height / 2); // cube1 (red) grows along mouse Y
+  cube2.dim = map(mouseX3D, 0, width / 2, 0, mouseX - width / 2); // cube2 (blue) grows along mouse X
   // constrain cube dimentions:
   cube1.dim = constrain(cube1.dim, 40, 300);
   cube2.dim = constrain(cube2.dim, 40, 300);
 
   //handle USER CUBE color variation
   // Map red chanel to vertical mouse:
-  if (mouseY3D > 0) {
+  if (mouseY3D > 0) { // positive Y values
     userCube.fill.r = map(mouseY3D, 0, 350, 0, 255);
-  } else {
+  } else { // positive X values
     userCube.fill.r = map(mouseY3D, 0, -350, 0, 255);
   };
   // Map blue chanel to horizontal mouse:
-  if (mouseX3D > 0) {
+  if (mouseX3D > 0) { // positive X values
     userCube.fill.b = map(mouseX3D, 0, 350, 0, 255);
-  } else {
+  } else { // negative X vales
     userCube.fill.b = map(mouseX3D, 0, -350, 0, 255);
   };
 
