0e7beff6b29aedd53b5c9983120694126e0a2e14
lmorv <84216711+lmorv@users.noreply.github.com>
2021-10-30 22:43:22 -0400

    inheritance: implemented polymorphism!!

--- experiments/inheritance experiments/js/script.js
+++ experiments/inheritance experiments/js/script.js
@@ -7,10 +7,8 @@ Following along with some experiments showcasing the concept of inheritance!
 
 "use strict";
 
-let cars = [];
+let vehicles = [];
 let numCars = 10;
-
-let motorcycles = [];
 let numMotorcycles = 10;
 
 /**
@@ -31,7 +29,7 @@ function setup() {
     let x = random(0, width);
     let y = random(0, height);
     let car = new Car(x, y);
-    cars.push(car);
+    vehicles.push(car);
   }
 
   // create the motorcycles
@@ -39,7 +37,7 @@ function setup() {
     let x = random(0, width);
     let y = random(0, height);
     let motorcycle = new Motorcycle(x, y);
-    motorcycles.push(motorcycle);
+    vehicles.push(motorcycle);
   }
 }
 
@@ -50,17 +48,12 @@ Description of draw()
 function draw() {
   background(0);
 
-  for (let i = 0; i < cars.length; i++) {
-    let car = cars[i];
-    car.move();
-    car.wrap();
-    car.display();
+  for (let i = 0; i < vehicles.length; i++) {
+    let vehicle = vehicles[i];
+    vehicle.move();
+    vehicle.wrap();
+    vehicle.display();
   }
 
-  for (let i = 0; i < motorcycles.length; i++) {
-    let motorcycle = motorcycles[i];
-    motorcycle.move();
-    motorcycle.wrap();
-    motorcycle.display();
-  }
+
 }
