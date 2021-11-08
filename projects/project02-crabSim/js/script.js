7eac5fbce444be7c9a7c58e28f0110fdbfe53ded
lmorv <84216711+lmorv@users.noreply.github.com>
2021-11-08 11:02:31 -0500

    crabSim: added crabSim files.

---
+++ projects/project02-crabSim/js/script.js
@@ -0,0 +1,32 @@
+/**
+Crab Simulator
+Leonardo Morales
+
+This is a crab simulator! It simulates crabs to the highest degree of sofistication.
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
