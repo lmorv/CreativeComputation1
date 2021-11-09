e856bb0fa20cb2f1e03c92a2547c9ec65c2a3261
lmorv <84216711+lmorv@users.noreply.github.com>
2021-11-08 21:59:57 -0500

    crabSim: sketched out state transitions in draw and state functions.

--- projects/project02-crabSim/crabSim-prototype/js/script.js
+++ projects/project02-crabSim/crabSim-prototype/js/script.js
@@ -6,6 +6,11 @@ This is a crab simulator! It simulates crabs to the highest degree of sofisticat
 */
 
 "use strict";
+
+let state = `simulation` // possible states are 'templateSlect', `crabEditor`, `confirmSelection`, `simulation`,`modelView`, `instructions`, `endScreen`, `simulationDestoyed`
+
+let crab; // The player object
+
 let crabTemplates = []; // store the 3 starting crab templates to be displayed in the start screen of the crab construction flow
 let consumables = []; // store all the consumables in this array (persons, schools, cars, and cities)
 // define the max number of each consumable item:
@@ -24,9 +29,6 @@ let numEndPoints = 1; // there's only one endpoint for now but it might be cool 
 let qBits = [];
 // define the number of  corrupted q-bits that break the simulation
 let critQBits = 164;
-
-let state = `simulation` // possible states are 'crabTemplateSlect', `crabEditor`, `confirmSelection`, `simulation`,`model view`, `instructions`, `endScreen`, `simulationDestoyed`
-
 
 /**
 preload() loads the game assets into variables for later use.
@@ -51,4 +53,54 @@ Description of draw()
 function draw() {
   background(0);
 
+  if (state === `templateSlect`) {
+    templateSlect();
+  } else if (state === `crabEditor`) {
+    crabEditor();
+  } else if (state === `confirmSelection`) {
+    confirmSelection();
+  } else if (state === `simulation`) {
+    simulation();
+  } else if (state === `modelView`) {
+    modelView();
+  } else if (state === `instructions`) {
+    instructions();
+  } else if (state === `endScreen`) {
+    endScreen();
+  } else if (`simulationDestoyed`) {
+    simulationDestoyed();
+  }
+
+}
+
+function templateSlect() {
+  //call the diplay method for the template select screen
+}
+
+function crabEditor() {
+  // display the selected crab template, check mouse position, allow user to edit head, carapace, and abdomen.
+}
+
+function confirmSelection() {
+  // display `confirm crab selection` message, check for mouse pess to transition to gameplay
+}
+
+function simulation() {
+  // call crab move() method, and all relevant dispay, and behavioural/ conditional game object methods
+}
+
+function modelView() {
+  // call display method for full screen model view
+}
+
+function instructions() {
+  // display instructions and controlls screen
+}
+
+function endScreen() {
+  // display `simulation successfull` message if crab overlaps the maze endPoint. Overlap is checked in simulation()
+}
+
+function simulationDestoyed() {
+  // display `simulation destroyed` message if corrupted q-bits reach the breaking threshold. Condition checked during simulation()
 }
