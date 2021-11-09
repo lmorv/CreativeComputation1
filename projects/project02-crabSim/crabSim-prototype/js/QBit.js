598c76fc8c64ab0f0d2745f1f6c1693096af893d
lmorv <84216711+lmorv@users.noreply.github.com>
2021-11-09 11:48:03 -0500

    crabSim: sketched out q-bit class.

---
+++ projects/project02-crabSim/crabSim-prototype/js/QBit.js
@@ -0,0 +1,21 @@
+class QBit {
+  constructor() {
+    this.x = x;
+    this.y = y;
+    this.size = 20;
+    this.fill = 0;
+  }
+
+  spawnQBit() {
+    // spawn QBits over time
+  }
+
+  // check corruption level here or in the main script?
+  checkCorruption() {
+    // check for max q-bits threshold value and trigger fail condition
+  }
+
+  dispay() {
+    // display a rotating black box in a random position.
+  }
+}
